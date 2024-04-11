6dab6129c37d179f23024ce43c2726e6fc517858
Alvin Pratama <palveeen22@gmail.com>
2024-04-11 10:00:10 +0700

    feat: added bottom sheet

--- App.tsx
+++ App.tsx
@@ -1,130 +1,172 @@
-import React from 'react';
-import type { PropsWithChildren } from 'react';
-import ReactNativeBiometrics, { BiometryTypes } from 'react-native-biometrics';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useCallback, useRef, useState, useMemo } from 'react';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { SafeAreaView, View, Text, StyleSheet, Image, Button, ScrollView } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App = (): React.ReactElement => {
+  const snapPoints = useMemo(() => ['25%', '50%', '90%'], []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View style={styles.viewBox}>
-          <Text>Open</Text>
-          <TouchableOpacity
-            onPress={async () => {
-              const rnBiometrics = new ReactNativeBiometrics();
-
-              const { available, biometryType } =
-                await rnBiometrics.isSensorAvailable();
-
-              if (!available || biometryType !== BiometryTypes.FaceID) {
-                Alert.alert(
-                  'Oops!',
-                  'Face ID is not available on this device.',
-                );
-                return;
-              }
-
-              const userId = await AsyncStorage.getItem('userId');
-
-              if (!userId) {
-                Alert.alert(
-                  'Oops!',
-                  'You have to sign in using your credentials first to enable Face ID.',
-                );
-                return;
-              }
-
-              const timestamp = Math.round(
-                new Date().getTime() / 1000,
-              ).toString();
-              const payload = `${userId}__${timestamp}`;
-
-              const { success, signature } = await rnBiometrics.createSignature(
-                {
-                  promptMessage: 'Sign in',
-                  payload,
-                },
-              );
-
-              if (!success) {
-                Alert.alert(
-                  'Oops!',
-                  'Something went wrong during authentication with Face ID. Please try again.',
-                );
-                return;
-              }
-
-              const { status, message } = await verifySignatureWithServer({
-                signature,
-                payload,
-              });
-
-              if (status !== 'success') {
-                Alert.alert('Oops!', message);
-                return;
-              }
-
-              Alert.alert('Success!', 'You are successfully authenticated!');
-            }}>
-            <View style={styles.btnSecondary}>
-              <MaterialCommunityIcons
-                color="#000"
-                name="face-recognition"
-                size={22}
-                style={{ marginRight: 12 }}
-              />
-
-              <Text style={styles.btnSecondaryText}>Face ID</Text>
-
-              <View style={{ width: 34 }} />
+    <GestureHandlerRootView style={styles.containerFull}>
+      <SafeAreaView>
+        <View style={styles.padding}>
+          <View style={styles.boxJustify}>
+            <View style={styles.column}>
+              <Text style={styles.textBlack}>улица аделя кутуя, 82</Text>
+              <Text style={styles.textGray}>Доставка 15 минут</Text>
             </View>
-          </TouchableOpacity>
+            <Text style={{ fontSize: 35 }}>🧑🏼‍🦱</Text>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </SafeAreaView>
+      <BottomSheet snapPoints={snapPoints}>
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+          {[...Array(4)].map((_, index) => (
+            <View key={index}>
+              <Text style={styles.sheetHeadline}>Awesome Bottom Sheet 🎉</Text>
+              <View style={styles.gridCard}>
+                {[...Array(4)].map((_, index) => (
+                  <View key={index} style={styles.cardSkeleton}></View>
+                ))}
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 }
 
+export default App;
+
+
 const styles = StyleSheet.create({
+  containerFull: {
+    flex: 1,
+    backgroundColor: "#141414"
+  },
+  imageContainer: {
+    width: "80%",
+    height: "80%"
+  },
+  container2: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#141414',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  padding: {
+    padding: 14
+  },
+  boxJustify: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  column: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    gap: 4
+  },
   viewBox: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
+  textGray: {
+    color: "#808080",
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  textBlack: {
+    color: "#fff",
+    fontSize: 15,
+    fontWeight: "600",
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+  },
+  bottomSheet: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  sheetHeadline: {
+    fontSize: 18,
+    fontWeight: '600',
+    padding: 10
+  },
+  gridCard: {
+    // flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap"
+  },
+  cardSkeleton: {
+    backgroundColor: "#808080",
+    margin: 5,
+    padding: 5,
+    height: 120,
+    width: 120,
+    borderRadius: 12
+  },
 });
 
-export default App;
+// const styles = StyleSheet.create({
+//   containerFull: {
+//     flex: 1,
+//     backgroundColor: "#141414"
+//   },
+//   padding: {
+//     padding: 14
+//   },
+//   boxJustify: {
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//   },
+//   column: {
+//     flexDirection: "column",
+//     justifyContent: "center",
+//     alignItems: "flex-start",
+//     gap: 4
+//   },
+//   textGray: {
+//     color: "#808080",
+//     fontSize: 16,
+//     fontWeight: "700",
+//   },
+//   textBlack: {
+//     color: "#fff",
+//     fontSize: 15,
+//     fontWeight: "600",
+//   },
+//   sheetHeadline: {
+//     fontSize: 18,
+//     fontWeight: '600',
+//     padding: 10
+//   },
+//   gridCard: {
+//     flexDirection: "row",
+//     flexWrap: "wrap"
+//   },
+//   cardSkeleton: {
+//     backgroundColor: "#808080",
+//     margin: 5,
+//     padding: 5,
+//     height: 120,
+//     width: 120,
+//     borderRadius: 12
+//   },
+//   contentContainer: {
+//     paddingVertical: 20,
+//     paddingHorizontal: 10,
+//     flexGrow: 1
+//   }
+// });
